cbb22a9c763998f6d9a6860401f450064fbd7f01
JCATMAN <joseluis3774060@gmail.com>
2021-07-28 09:20:58 -0500

    fix: relations

--- src/service/ChatService.js
+++ src/service/ChatService.js
@@ -3,8 +3,10 @@ import db from "../db/models";
 
 export default class ChatService {
   async findAll() {
-    const { chats } = db;
-    return await chats.findAll();
+    const { chats, messages, admin, contacts } = db;
+    return await chats.findAll({
+      include: [{ model: messages }, { model: admin }, { model: contacts }],
+    });
   }
 
   async findOrCreate(contactId, adminId) {
